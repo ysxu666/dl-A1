8954764c4fb31417b1f76dc1e8acf84820fe8a57
ysxu666 <70496853+ysxu666@users.noreply.github.com>
2024-02-27 10:29:19 +0800

    Update model.py

--- HKU-DASC7606-A1-main/retinanet/model.py
+++ HKU-DASC7606-A1-main/retinanet/model.py
@@ -193,10 +193,25 @@ class ResNet(nn.Module):
 # resnet152: [3, 8, 36, 3]
 ############################################
 
-def resnet101(num_classes, pretrained=False, **kwargs):
-    model = ResNet(num_classes, Bottleneck, [2, 2, 2, 2], **kwargs)
+def resnet18(num_classes, pretrained=False, **kwargs):
+    """Constructs a ResNet-18 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(num_classes, BasicBlock, [2, 2, 2, 2], **kwargs)
     if pretrained:
         model.load_state_dict(model_zoo.load_url(model_urls['resnet18'], model_dir='.'), strict=False)
+    return model
+
+
+def resnet34(num_classes, pretrained=False, **kwargs):
+    """Constructs a ResNet-34 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(num_classes, BasicBlock, [3, 4, 6, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet34'], model_dir='.'), strict=False)
     return model
 def resnet50(num_classes, pretrained=False, **kwargs):
     model = ResNet(num_classes, Bottleneck, [3, 4, 6, 3], **kwargs)
@@ -212,4 +227,12 @@ def resnet101(num_classes, pretrained=False, **kwargs):
     return model
 
 
-
+def resnet152(num_classes, pretrained=False, **kwargs):
+    """Constructs a ResNet-152 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(num_classes, Bottleneck, [3, 8, 36, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet152'], model_dir='.'), strict=False)
+    return model
