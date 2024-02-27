db3c7d72e95f7ccc84cb37c81f1eb564c7e84b8b
ysxu666 <70496853+ysxu666@users.noreply.github.com>
2024-02-27 10:27:36 +0800

    Update train.py

--- HKU-DASC7606-A1-main/train.py
+++ HKU-DASC7606-A1-main/train.py
@@ -44,6 +44,8 @@ def main(args=None):
         retinanet = model.resnet50(num_classes=dataset_train.num_classes(), pretrained=True)
     elif parser.depth == 101:
         retinanet = model.resnet101(num_classes=dataset_train.num_classes(), pretrained=True)
+    elif parser.depth == 152:
+        retinanet = model.resnet152(num_classes=dataset_train.num_classes(), pretrained=True)        
     else:
         raise ValueError('Unsupported model depth')
 
