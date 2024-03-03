bf3faae77f2c3b9c89d794834391ef5060df4256
ysxu666 <70496853+ysxu666@users.noreply.github.com>
2024-03-03 21:51:20 +0800

    Update vis.py

--- HKU-DASC7606-A1-main/vis.py
+++ HKU-DASC7606-A1-main/vis.py
@@ -7,11 +7,22 @@ import numpy as np
 from skimage import io
 import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 from torchvision import transforms
 from retinanet.dataloader import CocoDataset, Resizer, Normalizer
 
+
+global label_list 
+global color_list
+
+label_list = ['bird', 'bus', 'car', 'cat', 'dog']
+color_list = ['purple', 'blue', 'red', 'green', 'yellow']
+
+
 def draw(imgroot, img, coco_true, coco_pred=None):
+    global label_list 
+
     I = io.imread(os.path.join(imgroot, img['file_name']))
     plt.plot()
     plt.axis('off')
@@ -19,11 +30,24 @@ def draw(imgroot, img, coco_true, coco_pred=None):
     plt.imshow(I, aspect='equal')
     annids = coco_true.getAnnIds(imgIds=img['id'])
     anns = coco_true.loadAnns(annids)
-    coco_true.showAnns(anns, draw_bbox=True)
+    for ann in anns:
+        x, y, w, h = ann['bbox']
+        category = ann['category_id']
+        plt.text(x, y, label_list[int(category) - 1], fontsize = 12)
+        rect=mpatches.Rectangle((x, y),w,h, 
+                        fill=False,
+                        color=color_list[int(category) - 1],
+                       linewidth=2)
+        plt.gca().add_patch(rect)
+
     if coco_pred is not None:
         annids = coco_pred.getAnnIds(imgIds=img['id'])
         anns = coco_pred.loadAnns(annids)
-        coco_pred.showAnns(anns, draw_bbox=True)
+        for ann in anns:
+            x, y, w, h = ann['bbox']
+            category = ann['category_id']
+            plt.text(x, y, "pred_" + label_list[int(category) - 1], fontsize = 12)
+        coco_pred.showAnns(anns, draw_bbox=False)
 
 def main(args=None):
     parser = argparse.ArgumentParser(description='Simple training script for training a RetinaNet network.')
@@ -48,6 +72,7 @@ def main(args=None):
     imgids = coco_true.getImgIds(catIds=catids)
 
     # You can set to a certain id to visualize same images for comparison
+
     img = coco_true.loadImgs(imgids[np.random.randint(0, len(imgids))])[0]
 
     plt.figure()
